436adff7ee151a4b35e1cfecfe3061c07e8c5104
Aura-vLife <ombre147@gmail.com>
2020-10-20 03:22:20 +0200

    Update connect.php

--- inc/connect.php
+++ inc/connect.php
@@ -3,5 +3,5 @@ $user = 'tydoo';
 $pass = 'fq6EDJ]dB*&/Y)qS,6F]nK(7g$BNt)cx|kr!JFX+'; 
 $db = 'web'; 
 $mysqli = new mysqli('localhost', $user, $pass, $db) or die("Unable to connect"); 
-$mysqli->set_charset('utf8');
+$mysqli->set_charset('utf8mb4');
 ?>
